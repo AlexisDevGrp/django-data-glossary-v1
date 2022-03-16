92063ae87094198ba3be3c90519826fe671555ab
Alexis <AlexisDevGrp@gmail.com>
2022-03-16 08:02:50 +0100

    Add Customer admin model

--- datagloss/urls.py
+++ datagloss/urls.py
@@ -6,3 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Clearstream Data Layer Object Glossary Administration"
+admin.site.site_title = "CDL Objects Admin Portal"
+admin.site.index_title = "Welcome to the CDL Objects administration Portal"
