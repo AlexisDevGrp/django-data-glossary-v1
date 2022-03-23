51e64eee75a99276e457d8a1771d067cede012c3
Alexis <AlexisDevGrp@gmail.com>
2022-03-23 10:24:56 +0100

    Adding test pytest

--- datagloss/settings/development.py
+++ datagloss/settings/development.py
@@ -1,14 +1,14 @@
 from .base import *
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST ='smtp.mailtrap.io'
-EMAIL_HOST_USER ='9970a6ff348c23'
-EMAIL_HOST_PASSWORD ='1a0aa24a966cc6'
-EMAIL_PORT ='2525'
-DOMAIN='localhost:8000'
-DEFAULT_FROM_EMAIL = 'alexisdevgrp@gmail.com'
-DOMAIN = 'CDL'
-SITE_NAME = 'CDL Business Object Gloassary'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mailtrap.io"
+EMAIL_HOST_USER = "9970a6ff348c23"
+EMAIL_HOST_PASSWORD = "1a0aa24a966cc6"
+EMAIL_PORT = "2525"
+DOMAIN = "localhost:8000"
+DEFAULT_FROM_EMAIL = "alexisdevgrp@gmail.com"
+DOMAIN = "CDL"
+SITE_NAME = "CDL Business Object Gloassary"
 
 DATABASES = {
     "default": {
