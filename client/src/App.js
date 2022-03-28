19276760754aca17e4c0483438162011ac7a21e4
Alexis <AlexisDevGrp@gmail.com>
2022-03-28 06:04:18 +0200

    Added client react first part

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>CDL Business Object Glossary</h1>
+        <p> The real test starts here ...</p>
+    </div>
+  );
+}
+
+export default App;
